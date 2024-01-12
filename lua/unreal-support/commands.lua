52d95e2caa64bb6880ddbdc49fea96cc8361bc77
Sam Collier <samcollier916@gmail.com>
2024-01-12 17:39:20 +0000

    get usp working on linux

--- lua/unreal-support/commands.lua
+++ lua/unreal-support/commands.lua
@@ -1,34 +1,69 @@
 local M = {}
 
+-- path separator. "/" for linux, "\" for windows
+local p = package.config:sub(1, 1)
+
+local build_script = nil
+
 local function run_terminal_command(command)
 	vim.cmd("term" .. command)
 end
 
+local function build_script_path()
+	if not build_script then
+		local usp = require("unreal-support")
+		if p == "\\" then
+			build_script = usp.engine_path .. "\\Engine\\Build\\BatchFiles\\Build.bat"
+		else
+			build_script = usp.engine_path .. "/Engine/Build/BatchFiles/Linux/Build.sh"
+		end
+	end
+	return build_script
+end
+
 M.buildEditor = function()
 	local usp = require("unreal-support")
-	local command = "\"" .. usp.engine_path .. "\\Engine\\Build\\BatchFiles\\Build.bat\" " ..
-		"-project=\"" ..
+	local platform = "Win64"
+	if p == "/" then
+		platform = "Linux"
+	end
+	local command = build_script_path() ..
+		" -project=" ..
 		usp.project_path ..
-		"\\" .. usp.project_name .. ".uproject\" -game -engine " .. usp.project_name .. "Editor Win64 Development"
+		p ..
+		usp.project_name .. ".uproject -game -engine " .. usp.project_name .. "Editor " .. platform .. " Development"
 	run_terminal_command(command)
 end
 
 M.buildGame = function()
 	local usp = require("unreal-support")
-	local command = "\"" .. usp.engine_path .. "\\Engine\\Build\\BatchFiles\\Build.bat\" " ..
-		"-project=\"" ..
+	local platform = "Win64"
+	if p == "/" then
+		platform = "Linux"
+	end
+	local command = build_script_path() ..
+		" -project=" ..
 		usp.project_path ..
-		"\\" .. usp.project_name .. ".uproject\" -game -engine " .. usp.project_name .. " Win64 Development"
+		p ..
+		usp.project_name .. ".uproject -game -engine " .. usp.project_name .. " " .. platform .. " Development"
 	run_terminal_command(command)
 end
 
 M.gen = function()
 	local usp = require("unreal-support")
-	local command = "\"" .. usp.engine_path .. "\\Engine\\Build\\BatchFiles\\Build.bat\" " ..
-		"-mode=GenerateClangDatabase -project=\"" ..
+	local platform = "Win64"
+	local move = "move"
+	if p == "/" then
+		platform = "Linux"
+		move = "mv"
+	end
+	local command = build_script_path() ..
+		" -mode=GenerateClangDatabase -project=" ..
 		usp.project_path ..
-		"\\" .. usp.project_name .. ".uproject\" -game -engine " .. usp.project_name .. "Editor Win64 Development" ..
-		" && move \"" .. usp.engine_path .. "\\compile_commands.json\" \"" .. usp.project_path .. "\""
+		p ..
+		usp.project_name ..
+		".uproject -game -engine " .. usp.project_name .. "Editor " .. platform .. " Development" ..
+		" && " .. move .. " \"" .. usp.engine_path .. p .. "compile_commands.json\" \"" .. usp.project_path .. "\""
 	run_terminal_command(command)
 end
 
@@ -40,10 +75,15 @@ end
 
 M.clean = function()
 	local usp = require("unreal-support")
-	local command = "\"" .. usp.engine_path .. "\\Engine\\Build\\BatchFiles\\Build.bat\" " ..
+	local platform = "Win64"
+	if p == "/" then
+		platform = "Linux"
+	end
+	local command = "\"" .. build_script_path() .. "\" " ..
 		"-project=\"" ..
 		usp.project_path ..
-		"\\" .. usp.project_name .. ".uproject\" -game -engine " .. usp.project_name .. "Editor Win64 Development -Clean"
+		p ..
+		usp.project_name .. ".uproject\" -game -engine " .. usp.project_name .. " " .. platform .. " Development -Clean"
 	run_terminal_command(command)
 end
 
